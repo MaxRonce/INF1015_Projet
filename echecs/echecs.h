667f862661d06d51ca94ed536e705e082d8be5e7
Nada Alami <nadaalami2002@gmail.com>
2022-04-09 02:38:05 -0400

    se mefier aux instructions du td6

--- echecs/echecs.h
+++ echecs/echecs.h
@@ -23,65 +23,56 @@ class Echiquier {
 class Piece {
 public:
 	virtual bool mouvementValide(std::pair<int, int>& mouvement, int pas) = 0;
+	virtual void deplacer(const int x, const int y) = 0;
+
 private:
+
 	int x_ = 0;
 	int y_ = 0;
 	std::pair<int, int> mouvement_;
+	char piece = 'P';
 };
 
 class Roi : public Piece {
 public:
-	Roi(int a, int b) : x_(a), y_(b) {};
+	Roi(int a, int b) : x_(a), y_(b) { ++compteurInstance; };
 	bool mouvementValide(std::pair<int, int>& mouvement, int pas) override;
-
-	/*  void avancer();
-	  void reculer();
-	  void avancerDroite();
-	  void avancerGauche();
-	  void diagonaleDroite();
-	  void diagonaleGauche();
-	  void diagonaleHaut();
-	  void diagonaleBas();*/
+	void deplacer(const int x, const int y) override;
+	~Roi() { --compteurInstance; };
 private:
 
 	int x_ = 0;
 	int y_ = 0;
 	std::pair<int, int> mouvement_;
+	char piece = '$';
+	static int inline compteurInstance;
 
 };
 
 class Reine : public Piece {
 public:
 	bool mouvementValide(std::pair<int, int>& mouvement, int pas) override;
-	/*  void avancer(const int x, const int y);
-	  void reculer(const int x, const int y);
-	  void avancerDroite(const int x, const int y);
-	  void avancerGauche(const int x, const int y);
-	  void diagonaleDroite(const int x, const int y);
-	  void diagonaleGauche(const int x, const int y);
-	  void diagonaleHaut(const int x, const int y);
-	  void diagonaleBas(const int x, const int y);*/
+	void deplacer(const int x, const int y) override;
 
 private:
 
 	int x_ = 0;
 	int y_ = 0;
 	std::pair<int, int> mouvement_;
+	char piece = 'R';
 
 };
 
 class Tour : public Piece {
 public:
 	bool mouvementValide(std::pair<int, int>& mouvement, int pas) override;
-	/*  void avancer();
-	  void reculer();
-	  void avancerDroite();
-	  void avancerGauche ();*/
-
+	void deplacer(const int x, const int y) override;
+	
 private:
 
 	int x_ = 0;
 	int y_ = 0;
 	std::pair<int, int> mouvement_;
+	char piece = 'T';
 
 };
