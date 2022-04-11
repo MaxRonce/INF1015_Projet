bad699ddbde8bea745e3844097e98613bd71e8f3
mrx <nada.alami-chentoufi@polymtl.ca>
2022-04-11 02:22:28 -0400

    deplacer() finis, affichage a tester

--- echecs/echecs.cpp
+++ echecs/echecs.cpp
@@ -2,23 +2,24 @@
 #include <algorithm>
 using namespace std;
 
-Roi::Roi(int a, int b) : x_(a), y_(b)
+
+Roi::Roi()
 {
 	++compteurInstance;
 	if (compteurInstance > 2)
-		throw InstancesRoiException("PLus que 2 instances de Roi"); //Oublie pas de mettre le try catch dans le main
+		throw InstancesRoiException("Plus que 2 instances de Roi"); //Oublie pas de mettre le try catch dans le main
 };
 
 Roi::~Roi() { --compteurInstance; };
 
-bool Roi::mouvementValide(pair<int, int>& mouvement, int pas) {
+bool Roi::mouvementValide(int pas) {
 
 	std::vector<pair<int, int>> mouvementsAutorises{ {x_ , y_ - pas }, {x_ , y_ + pas }, {x_ - pas, y_}, {x_ + pas, y_ }, {x_ - pas, y_ - pas }, {x_ + pas, y_ + pas }, {x_ - pas, y_ + pas}, {x_ + pas, y_ - pas } };
-	if (find(mouvementsAutorises.begin(), mouvementsAutorises.end(), mouvement) != mouvementsAutorises.end()) {
+	if (find(mouvementsAutorises.begin(), mouvementsAutorises.end(), position) != mouvementsAutorises.end()) {
 		return true;
 	}
 	else {
-		if (*mouvementsAutorises.end() == mouvement) {
+		if (*mouvementsAutorises.end() == position) {
 			return true;
 		}
 		return false;
@@ -26,31 +27,35 @@ bool Roi::mouvementValide(pair<int, int>& mouvement, int pas) {
 
 };
 
-void Roi::deplacer(const int x, const int y) {
-	mouvement_.first+=x;
-	mouvement_.second+=y;
+void Roi::deplacer(pair<char, int> pair) {
+	auto coordonnees = map.find(pair.first);
+	position.first = coordonnees->second;
+	position.second = pair.second;
 
-}
+};
 
-bool Reine::mouvementValide(pair<int, int>& mouvement, int pas) {
+
+bool Reine::mouvementValide(int pas) {
 
 	return true;
 
 };
 
-void Reine::deplacer(const int x, const int y) {
-	mouvement_.first += x;
-	mouvement_.second += y;
-}
+void Reine::deplacer(pair<char, int> pair) {
+	auto coordonnees = map.find(pair.first);
+	position.first = coordonnees->second;
+	position.second = pair.second;
+};
 
-bool Tour::mouvementValide(pair<int, int>& mouvement, int pas) {
+
+bool Tour::mouvementValide(int pas) {
 
 	std::vector<pair<int, int>> mouvementsAutorises{ { x_ , y_ - pas }, { x_ , y_ + pas }, { x_ - pas, y_ }, { x_ + pas, y_ } };
-	if (find(mouvementsAutorises.begin(), mouvementsAutorises.end(), mouvement) != mouvementsAutorises.end()) {
+	if (find(mouvementsAutorises.begin(), mouvementsAutorises.end(), position) != mouvementsAutorises.end()) {
 		return true;
 	}
 	else {
-		if (*mouvementsAutorises.end() == mouvement) {
+		if (*mouvementsAutorises.end() == position) {
 			return true;
 		}
 		return false;
@@ -58,7 +63,78 @@ bool Tour::mouvementValide(pair<int, int>& mouvement, int pas) {
 
 };
 
-void Tour::deplacer(const int x, const int y) {
-	mouvement_.first += x;
-	mouvement_.second += y;
-}
+void Tour::deplacer(pair<char, int> pair) {
+	auto coordonnees = map.find(pair.first);
+	position.first = coordonnees->second;
+	position.second = pair.second;
+};
+
+Echiquier::Echiquier() {
+	char board2[8][8] = {
+		//  H    G    F    E    D    C    B    A 
+		 { 'T', '0', '0', 'K', 'Q', '0', '0', 'T',},//1
+		 { '0', '0', '0', '0', '0', '0', '0', '0' },//2
+		 { '0', '0', '0', '0', '0', '0', '0', '0' },//3
+		 { '0', '0', '0', '0', '0', '0', '0', '0' },//4
+		 { '0', '0', '0', '0', '0', '0', '0', '0' },//5
+		 { '0', '0', '0', '0', '0', '0', '0', '0' },//6
+		 { '0', '0', '0', '0', '0', '0', '0', '0' },//7
+		 { 'T', '0', '0', 'K', 'Q', '0', '0', 'T' },//8
+	};
+
+	for (int i = 0; i < 8; ++i) {
+		for (int j = 0; j < 8; ++j) {
+			board[i][j] = board2[i][j];
+		}
+	}
+	roiA.deplacer({ 'E', 1 });
+	roiB.deplacer({ 'E', 8 });
+	reineA.deplacer({ 'D', 1 });
+	reineB.deplacer({ 'D', 8 });
+	tourAA.deplacer({ 'H', 1 });
+	tourAB.deplacer({ 'A', 1 });
+	tourBA.deplacer({ 'H', 8 });
+	tourBB.deplacer({ 'A', 8 });
+
+};
+void Echiquier::modifierBoard(int x, int y, char symbole){
+	board[y][x] = symbole;
+};
+
+void Echiquier::afficher() {
+	//synchroniserBoard();
+	std::cout << "  H G F E D C B A \n";
+	for (int i = 0; i < 8; ++i) {
+		std::cout << i + 1 << "|";
+		for (int j = 0; j < 8; ++j) {
+			std::cout << board[i][j] << " ";
+		}
+		std::cout << "\n";
+	}
+};
+
+//const shared_ptr<Roi> Echiquier::getRoiA() const {
+//	shared_ptr<Roi> ptrRoi = make_shared<Roi>(roiA);
+//	return ptrRoi;
+//};
+//
+//const shared_ptr<Reine> Echiquier::getReineA() const {
+//	shared_ptr<Reine> ptrReine = make_shared<Reine>(reineA);
+//	return ptrReine;
+//};
+//
+//const shared_ptr<Tour> Echiquier::getTourA() const {
+//	shared_ptr<Tour> ptrRoi = make_shared<Tour>(tourAA);
+//	return ptrRoi;
+//};
+void Echiquier::synchroniserBoard() {
+
+	modifierBoard(reineA.position.first, reineA.position.second, reineA.symbole);
+	modifierBoard(reineB.position.first, reineB.position.second, reineB.symbole);
+	modifierBoard(roiA.position.first, roiA.position.second, roiA.symbole);
+	modifierBoard(roiB.position.first, roiB.position.second, roiB.symbole);
+	modifierBoard(tourAA.position.first, tourAA.position.second, tourAA.symbole);
+	modifierBoard(tourAB.position.first, tourAB.position.second, tourAB.symbole);
+	modifierBoard(tourBA.position.first, tourBA.position.second, tourBA.symbole);
+	modifierBoard(tourBB.position.first, tourBB.position.second, tourBB.symbole);
+};
--- echecs/echecs.h
+++ echecs/echecs.h
@@ -2,54 +2,51 @@
 #include <vector>
 #include <utility>
 #include <stdexcept>
+#include <map>
 
 
+//class JeuEchec {
+//	int nbPiecePrise;
+//	std::vector<char> piecesPrises;
+//};
 
-class JeuEchec {
-	int nbPiecePrise;
-	std::vector<char> piecesPrises;
-};
+//class Case {
+//public:
+//	bool occupee; //True si occupee
+//private:
+//	int x, y; //coordones
+//
+//};
 
-class Case {
-public:
-	bool occupee; //True si occupee
-private:
-	int x, y; //coordones
-
-};
-
-class Echiquier {
-	char echiquier[8][8]; //[colonne] [ligne]
-};
 
 
 class Piece {
 public:
-	virtual bool mouvementValide(std::pair<int, int>& mouvement, int pas) = 0;
-	virtual void deplacer(const int x, const int y) = 0;
+	virtual bool mouvementValide(int pas) = 0;
+	virtual void deplacer(std::pair<char, int> pair) = 0;
+	std::map<char, int> map{ {'A', 8}, {'B', 7},{'C', 6},{'D', 5},{'E', 4}, {'F', 3}, {'G', 2},{'H', 1}};
 
 private:
-
 	int x_ = 0;
 	int y_ = 0;
-	std::pair<int, int> mouvement_;
-	char piece = 'P';
+	std::pair<int, int> position;
+	char symbole = 'P';
 };
 
 class Roi : public Piece {
 public:
-	Roi(int a, int b);
-	bool mouvementValide(std::pair<int, int>& mouvement, int pas) override;
-	void deplacer(const int x, const int y) override;
+	Roi();
+	bool mouvementValide(int pas) override;
+	void deplacer(std::pair<char, int> pair) override;
 	~Roi();
+	static int inline compteurInstance;
 
+	std::pair<int, int> position;
+	char symbole = 'K';
 private:
 
-	int x_ = 0;
-	int y_ = 0;
-	std::pair<int, int> mouvement_;
-	char piece = 'K';
-	static int inline compteurInstance;
+	int x_ = position.first;
+	int y_ = position.second;
 
 };
 
@@ -61,28 +58,50 @@ public:
 
 class Reine : public Piece {
 public:
-	bool mouvementValide(std::pair<int, int>& mouvement, int pas) override;
-	void deplacer(const int x, const int y) override;
+	bool mouvementValide(int pas) override;
+	void deplacer(std::pair<char, int> pair) override;
+
+	std::pair<int, int> position = {};
+	char symbole = 'Q';
 
 private:
 
-	int x_ = 0;
-	int y_ = 0;
-	std::pair<int, int> mouvement_;
-	char piece = 'Q';
-
+	int x_ = position.first;
+	int y_ = position.second;
 };
 
 class Tour : public Piece {
 public:
-	bool mouvementValide(std::pair<int, int>& mouvement, int pas) override;
-	void deplacer(const int x, const int y) override;
-	
+	bool mouvementValide(int pas) override;
+	void deplacer(std::pair<char, int> pair) override;
+
+	std::pair<int, int> position;
+	char symbole = 'T';
 private:
 
-	int x_ = 0;
-	int y_ = 0;
-	std::pair<int, int> mouvement_;
-	char piece = 'T';
+	int x_ = position.first;
+	int y_ = position.second;
+
+};
+
+class Echiquier {
+public:
+	Echiquier();
+	void modifierBoard(int x, int y, char symbole);
+	void synchroniserBoard();
+	void afficher();
+	/*const std::shared_ptr<Roi> getRoiA() const;
+	const std::shared_ptr<Reine> getReineA() const;
+	const std::shared_ptr<Tour> getTourA() const;*/
+private:
+	Roi roiA;
+	Roi roiB;
+	Reine reineA;
+	Reine reineB;
+	Tour tourAA;
+	Tour tourAB;
+	Tour tourBA;
+	Tour tourBB;
+	char board[8][8] = {}; //[colonne] [ligne]
 
 };
