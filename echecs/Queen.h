dced85f1970e3098308221479ccc2f0f1642baee
mrx <nada.alami-chentoufi@polymtl.ca>
2022-04-24 01:48:01 -0400

    transfert vers autre dossier

--- echecs/Queen.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Piece.h"
-
-class Queen : public Piece {
-public:
-	Queen();
-	bool validMove() override;
-	void move(char x, int y) override;
-};
--- echecs/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "Game.h"
-
-
-using namespace std;
-
-int main() {
-	Game game;
-	game.start;
-}
